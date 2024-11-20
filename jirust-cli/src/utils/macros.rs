26a1f09f943b516ce660333c557511495e56016e
Emanuele Panigati <ilpanich@gmail.com>
2024-11-20 23:51:12 +0100

    fix: fixed jira_doc_std_field macro
    Signed-off-by: Emanuele Panigati <ilpanich@gmail.com>

--- jirust-cli/src/utils/macros.rs
+++ jirust-cli/src/utils/macros.rs
@@ -1,6 +1,7 @@
 #[macro_export]
 macro_rules! jira_doc_std_field {
-    ($v:literal) => {
-        serde_json::json!({"body":{"content":[{"content":[{"text":$v,"type":"text"}],"type":"paragraph"}],"type":"doc","version":1}})
+    ($v:expr) => {
+        format!(
+            "{{\"body\":{{\"content\":[{{\"content\":[{{\"text\":\"{}\",\"type\":\"text\"}}],\"type\":\"paragraph\"}}],\"type\":\"doc\",\"version\":1}}}}", $v).as_str()
     }
 }
